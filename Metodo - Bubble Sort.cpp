80c12036cbe90c873553c6e3da5036c9b1a8ab05
maricielo <cielotapia198@gmail.com>
2025-09-04 20:36:50 -0500

    VERSION FINAL - Mostrar arreglo ordenado

--- Metodo - Bubble Sort.cpp
+++ Metodo - Bubble Sort.cpp
@@ -23,6 +23,30 @@ int main(){
 		if (i < max - 1)
 		cout << ", ";
 	}
+	cout << "]" << endl;
+	
+	cout << "\n---- Ordeando..." << endl;
+	
+	//Ahora utilizaremos el metodo bubble sort para ordenarlo de menor a mayor
+	
+	for(int i = 0; i < max; i++){
+		for(int j = 0; j < max - i - 1; j++){
+			if(myarray[j] > myarray[j + 1]){
+			int temp = myarray[j];
+			myarray[j] = myarray[j + 1];
+			myarray[j + 1] = temp;
+		}
+		}
+	}
+	cout << endl;
+	cout << "Este es tu arreglo ordenado: [";
+	
+	for(int i = 0; i < max; i++){
+		cout << myarray[i];
+		if (i < max - 1)
+		cout << ", ";
+	}
 	cout << "]";
+	
 	return 0;
 }
