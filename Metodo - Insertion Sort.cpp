924b81f9783e6b725ad6113761532a9b621b93ce
maricielo <cielotapia198@gmail.com>
2025-09-11 00:14:46 -0500

    Metodo insertion sort - version final: Mostrar arreglo ordenado

--- Metodo - Insertion Sort.cpp
+++ Metodo - Insertion Sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+void insertionSort(int* ,int );
 int main(){
 	int n;
 	
@@ -22,6 +23,30 @@ int main(){
 		cout << ", ";
 	}
 	cout << "]" << endl;
-	return 0;
 	
+	insertionSort(myarray, n);
+	cout << "El arreglo ordenado es: [";
+	
+	for (int i = 0; i < n; i++){
+		cout << myarray[i];
+		
+		if (i < n - 1)
+		cout << ", ";
+	}
+	cout << "]" << endl;
+	
+	return 0;
+}
+void insertionSort(int myarray[] ,int n){
+	for(int i = 1; i < n; i++){
+		int aux = myarray[i];
+		
+		int k = i - 1;
+		
+		while (k >= 0 && aux < myarray[k]){
+			myarray[k + 1] = myarray[k];
+			k = k - 1;
+		}
+		myarray[k + 1] = aux;
+	}
 }
